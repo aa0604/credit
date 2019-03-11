2ad883af63d6795d5db5c2c2dec3d2bc393503be
aa0604 <aa0604@sina.cn>
2019-03-11 17:25:47 +0800

    初始化

---
+++ Ali.php
@@ -0,0 +1,95 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xing.chen
+ * Date: 2019/3/11
+ * Time: 17:14
+ */
+
+namespace xing\credit;
+
+use xing\payment\sdk\aliPay\aop\AopClient;
+
+class Ali
+{
+
+    private $config;
+
+    private $params;
+
+    public $AopClient;
+
+    public $request;
+
+
+    /**
+     * 初始化
+     * @param $config
+     * @return AliPay
+     */
+    public static function init($config)
+    {
+        $class = new self();
+        $class->config = $config;
+        defined('AOP_SDK_WORK_DIR') ?: define("AOP_SDK_WORK_DIR", $config['logDir'] ?? sys_get_temp_dir() . '/');
+
+//        初始化支付宝和配置参数
+//        $class->AopClient = $class->getAopClient();
+//        返回本类自身
+        return $class;
+    }
+    /**
+     * @return AopClient
+     */
+    private function getAopClient()
+    {
+        $config = & $this->config;
+        $aopClient = new AopClient();
+        $aopClient->appId = $config['appId'];
+        $aopClient->rsaPrivateKey = $config['rsaPrivateKey'];  // 请填写开发者私钥去头去尾去回车，一行字符串
+        $aopClient->alipayrsaPublicKey = $config['alipayrsaPublicKey']; // 请填写支付宝公钥，一行字符串
+        $aopClient->signType = $config['signType'] ?? 'RSA2';  // 签名方式
+        $aopClient->format = $config['format'] ?? 'JSON';
+        $aopClient->charset = $config['charset'] ?? 'utf-8';
+
+        return $aopClient;
+    }
+
+    public function startInit($cardNumber, $name = '', $transactionId = '')
+    {
+
+        $set = PaymentSetMap::$set['aliPay'];
+        $linkedMerchantId = $set['linkedMerchantId'] ?? '';
+        $aop = $this->getAopClient();
+        $aop = new \xing\payment\sdk\aliPay\aop\AopClient ();
+//        $aop->gatewayUrl = 'https://openapi.alipay.com/gateway.do';
+//        $aop->appId = $set['appId'];
+//        $aop->rsaPrivateKey = $set['rsaPrivateKey'];
+//        $aop->alipayrsaPublicKey=$set['alipayrsaPublicKey'];
+        $aop->apiVersion = '1.0';
+//        $aop->signType = 'RSA2';
+//        $aop->postCharset='UTF-8';
+//        $aop->format='json';
+        $request = new \xing\payment\sdk\aliPay\aop\request\ZhimaCustomerCertificationInitializeRequest ();
+        $request->setBizContent("{" .
+            "\"transaction_id\":\"{$transactionId}\"," .
+            "\"product_code\":\"w1010100000000002978\"," .
+            "\"biz_code\":\"FACE\"," .
+            "\"identity_param\":\"{\\\"identity_type\\\":\\\"CERT_INFO\\\",\\\"cert_type\\\":\\\"IDENTITY_CARD\\\",\\\"cert_name\\\":\\\"{$cardNumber}\\\",\\\"cert_no\\\":\\\"{$cardNumber}\\\"}\"," .
+            "\"merchant_config\":\"{}\"," .
+            "\"ext_biz_param\":\"{}\"," .
+            "\"linked_merchant_id\":\"{$linkedMerchantId}\"," .
+            "\"face_contrast_picture\":\"xydasf==\"" .
+            "  }");
+        $result = $aop->execute ( $request);
+
+        $responseNode = str_replace(".", "_", $request->getApiMethodName()) . "_response";
+        $resultCode = $result->$responseNode->code;
+        er($result);
+        if(!empty($resultCode)&&$resultCode == 10000){
+            echo "成功";
+        } else {
+            echo "失败";
+        }
+    }
+}
